861b05609e106df4f4fca3565f7d30138c8db2d8
android <trevd1234@gmail.com>
2014-08-30 09:32:59 +0100

    Use 800x600 Bootanimation

--- cm.mk
+++ cm.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, device/broadcom/rpi/full_rpi.mk)
 
 # Inherit some common CM stuff.
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 600
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Overrides
